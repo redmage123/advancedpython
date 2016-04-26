4ce1dc8a91603b64bef5fe4a6b784d8805918e2e
Braun Brelin <bbrelin@gmail.com>
2016-04-26 14:33:42 +0100

    Added coroutine.py

---
+++ examples/coroutine.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+
+import urllib
+
+class Ticker(object):
+    def __init__(self):
+        pass
+
+    def consumer(self):
+       while True:
+            symbol = (yield)
+            print ("symbol = " + symbol)
+            urlstring = 'http://finance.yahoo.com/d/quotes.csv?format=json&s=\"%s\"&f=nab' % (symbol)
+            response = urllib.urlopen(urlstring)
+            for line in response: 
+                print line
+
+    def producer(self):
+        c = self.consumer()
+        c.send(None)
+        c.send("GOOG")
+
+
+t = Ticker()
+t.producer()
