0a6f038a1d22082b05b1bd6265c01a436bb785f8
bbrelin <bbrelin@gmail.com>
2016-08-20 16:52:04 +0100

    Updated doc

---
+++ labs/lab1/studentReg.py
@@ -0,0 +1,30 @@
+class Student(object):
+    def__init__(self):
+        pass
+
+    def getStudentDetails(self):
+        pass
+
+    def setStudentDetails(self):
+        pass
+
+
+class Course(object):
+
+    def __init__(self):
+        pass
+
+    def setCourseDetails(self):
+        pass
+
+    def getCourseDetails(self):
+        pass
+
+class studentReg(object):
+
+    def __init__():
+        pass
+
+    
+
+
